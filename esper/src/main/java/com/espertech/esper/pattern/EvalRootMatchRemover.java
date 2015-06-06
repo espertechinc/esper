5b9367fa00f569a4128105e3ef85d0c0f6dffe0b
bernhardttom <bernhardttom@yahoo.com>
2015-06-06 09:16:05 -0400

    minor service refactoring for pattern-remove

---
+++ esper/src/main/java/com/espertech/esper/pattern/EvalRootMatchRemover.java
@@ -0,0 +1,21 @@
+/**************************************************************************************
+ * Copyright (C) 2006-2015 EsperTech Inc. All rights reserved.                        *
+ * http://www.espertech.com/esper                                                          *
+ * http://www.espertech.com                                                           *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the GPL license       *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package com.espertech.esper.pattern;
+
+import com.espertech.esper.client.EventBean;
+
+import java.util.Set;
+
+/**
+ * Interface for a root pattern node for removing matches.
+ */
+public interface EvalRootMatchRemover
+{
+    public void removeMatch(Set<EventBean> matchEvent);
+}
