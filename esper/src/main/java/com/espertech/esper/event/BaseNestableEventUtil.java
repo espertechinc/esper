7209549246798c9cccbf65849db0c8f2089e786f
Nicolas Correard <nicolas.correard@misys.com>
2015-06-26 14:51:23 +0100

    fix import + non UTF-8 character in comment

--- esper/src/main/java/com/espertech/esper/event/BaseNestableEventUtil.java
+++ esper/src/main/java/com/espertech/esper/event/BaseNestableEventUtil.java
@@ -25,6 +25,7 @@ import com.espertech.esper.util.JavaClassHelper;
 
 import java.lang.reflect.Array;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
