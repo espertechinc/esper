a1765d7ce3e5c3a0e9e215108535241624df975f
Thomas Bernhardt <tom@espertech.com>
2019-06-03 10:36:40 -0400

    merge #149 enum method used on cast to collection not validating

--- common/src/main/java/com/espertech/esper/common/internal/epl/expression/dot/core/ExprDotNodeUtility.java
+++ common/src/main/java/com/espertech/esper/common/internal/epl/expression/dot/core/ExprDotNodeUtility.java
@@ -225,6 +225,9 @@ public class ExprDotNodeUtility {
             if (EnumMethodEnum.isEnumerationMethod(chainElement.getName()) && (!matchingMethod || methodTarget.isArray() || JavaClassHelper.isImplementsInterface(methodTarget, Collection.class))) {
                 EnumMethodEnum enumerationMethod = EnumMethodEnum.fromName(chainElement.getName());
                 ExprDotForgeEnumMethod eval = (ExprDotForgeEnumMethod) JavaClassHelper.instantiate(ExprDotForgeEnumMethod.class, enumerationMethod.getImplementation());
+                if (currentInputType instanceof ClassEPType && JavaClassHelper.isImplementsInterface(((ClassEPType) currentInputType).getType(), Collection.class) ) {
+                    currentInputType = EPTypeHelper.collectionOfSingleValue(Object.class);
+                }
                 eval.init(streamOfProviderIfApplicable, enumerationMethod, chainElement.getName(), currentInputType, chainElement.getParameters(), validationContext);
                 currentInputType = eval.getTypeInfo();
                 if (currentInputType == null) {
