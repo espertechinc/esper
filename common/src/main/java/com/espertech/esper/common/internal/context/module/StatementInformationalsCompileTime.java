603a2e29ea6a97d4e8846c1aecbb68e2cd8bc0d8
Thomas Bernhardt <bernhardttom@yahoo.com>
2019-06-03 18:49:15 -0400

    merge JFU-988-13370 find-annotation considers annotations of annotations

--- common/src/main/java/com/espertech/esper/common/internal/context/module/StatementInformationalsCompileTime.java
+++ common/src/main/java/com/espertech/esper/common/internal/context/module/StatementInformationalsCompileTime.java
@@ -252,7 +252,7 @@ public class StatementInformationalsCompileTime {
     }
 
     private CodegenExpression makeAuditProvider(CodegenMethod method, CodegenClassScope classScope) {
-        if (AnnotationUtil.hasAnnotation(annotations, Audit.class)) {
+        if (!AnnotationUtil.hasAnnotation(annotations, Audit.class)) {
             return publicConstValue(AuditProviderDefault.class, "INSTANCE");
         }
 
