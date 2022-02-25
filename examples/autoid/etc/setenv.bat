7d1501ecef048eae94b141321c799fa12ed6f10e
Thomas Bernhardt <tom@espertech.com>
2022-02-25 10:01:45 -0500

    merge fix autoid example dependencies

--- examples/autoid/etc/setenv.bat
+++ examples/autoid/etc/setenv.bat
@@ -13,17 +13,22 @@ if not exist "%JAVA_HOME%\bin\java.exe" (
 )
 
 set LIB_COMPILER=..\..\..\dependencies\compiler
+set LIB_COMMON_XMLXSD=..\..\..\dependencies\common-xmlxsd
 
 set CLASSPATH=.
 set CLASSPATH=%CLASSPATH%;..\target\classes
 set CLASSPATH=%CLASSPATH%;..\..\..\esper-common-8.8.0.jar
 set CLASSPATH=%CLASSPATH%;..\..\..\esper-compiler-8.8.0.jar
 set CLASSPATH=%CLASSPATH%;..\..\..\esper-runtime-8.8.0.jar
+set CLASSPATH=%CLASSPATH%;..\..\..\esper-common-xmlxsd-8.8.0.jar
 set CLASSPATH=%CLASSPATH%;%LIB_COMPILER%\slf4j-api-1.7.30.jar
 set CLASSPATH=%CLASSPATH%;%LIB_COMPILER%\slf4j-log4j12-1.7.30.jar
 set CLASSPATH=%CLASSPATH%;%LIB_COMPILER%\log4j-1.2.17.jar
 set CLASSPATH=%CLASSPATH%;%LIB_COMPILER%\antlr4-runtime-4.9.3.jar
 set CLASSPATH=%CLASSPATH%;%LIB_COMPILER%\commons-compiler-3.1.6.jar
 set CLASSPATH=%CLASSPATH%;%LIB_COMPILER%\janino-3.1.6.jar
+set CLASSPATH=%CLASSPATH%;%LIB_COMMON_XMLXSD%\xercesImpl-2.12.1.jar
+set CLASSPATH=%CLASSPATH%;%LIB_COMMON_XMLXSD%\xml-apis-1.4.01.jar
+
 
 :EOF
--- examples/autoid/etc/setenv.sh
+++ examples/autoid/etc/setenv.sh
@@ -17,17 +17,22 @@ then
 fi
 
 LIB_COMPILER=../../../dependencies/compiler
+LIB_COMMON_XMLXSD=../../../dependencies/common_xmlxsd
+
 
 CLASSPATH=.
 CLASSPATH=$CLASSPATH:../target/classes
 CLASSPATH=$CLASSPATH:../../../esper-common-8.8.0.jar
 CLASSPATH=$CLASSPATH:../../../esper-compiler-8.8.0.jar
 CLASSPATH=$CLASSPATH:../../../esper-runtime-8.8.0.jar
+CLASSPATH=$CLASSPATH:../../../esper-common-xmlxsd-8.8.0.jar
 CLASSPATH=$CLASSPATH:$LIB_COMPILER/slf4j-api-1.7.30.jar
 CLASSPATH=$CLASSPATH:$LIB_COMPILER/slf4j-log4j12-1.7.30.jar
 CLASSPATH=$CLASSPATH:$LIB_COMPILER/log4j-1.2.17.jar
 CLASSPATH=$CLASSPATH:$LIB_COMPILER/antlr4-runtime-4.9.3.jar
 CLASSPATH=$CLASSPATH:$LIB_COMPILER/commons-compiler-3.1.6.jar
 CLASSPATH=$CLASSPATH:$LIB_COMPILER/janino-3.1.6.jar
+CLASSPATH=$CLASSPATH:$LIB_COMMON_XMLXSD/xercesImpl-2.12.1.jar
+CLASSPATH=$CLASSPATH:$LIB_COMMON_XMLXSD/xml-apis-1.4.01.jar
 
 export CLASSPATH="$CLASSPATH"
